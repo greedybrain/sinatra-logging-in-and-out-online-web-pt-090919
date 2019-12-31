78b40924bcbd5a5945ab0f3b358ce2163e7c3837
Naya  Willis <greedybrain18@gmail.com>
2019-12-31 03:32:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-require_relative '../../config/environment'
+# require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
